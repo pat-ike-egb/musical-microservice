98ebf0a4c2c78c8e18445130138db268ec644341
pat ike egb <patrick.egbuchulam@gmail.com>
2023-06-12 23:14:10 -0400

    play audio in client

--- musical_microservice/client.py
+++ musical_microservice/client.py
@@ -1,6 +1,11 @@
 import grpc
+import pyaudio
 import services.generated.audio_server_pb2 as pb2
 import services.generated.audio_server_pb2_grpc as pb2_grpc
+
+NUM_CHANNELS = 2
+SAMPLE_WIDTH = 2
+SAMPLES_PER_SEC = 44100
 
 if __name__ == "__main__":
     host = "localhost"
@@ -14,6 +19,24 @@ if __name__ == "__main__":
 
     request = pb2.ConnectRequest()
 
-    responses: list[pb2.AudioData] = stub.connect(request)
-    for response in responses:
-        print(f"Hello from the server received your {len(response.data)}")
+    p = pyaudio.PyAudio()
+    # open stream
+    stream = p.open(
+        format=p.get_format_from_width(SAMPLE_WIDTH),
+        channels=2,
+        rate=44100,
+        output=True,
+    )
+
+    try:
+        responses = stub.connect(request)
+        for response in responses:
+            print(f"Received {len(response.data)} bytes from server")
+            stream.write(response.data)
+    finally:
+        # stop stream
+        stream.stop_stream()
+        stream.close()
+
+        # close PyAudio
+        p.terminate()
